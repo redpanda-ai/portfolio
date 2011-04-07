36173b698bdd5ed5a75e3b56054452c308f3318c
J. Andrew Key <joeandrewkey@gmail.com>
2011-04-07 09:37:15 -0700

    Added better comments for simple hadoop MR

--- java/hadoop/wordcount/WordCount.java
+++ java/hadoop/wordcount/WordCount.java
@@ -12,8 +12,8 @@ import org.apache.hadoop.util.*;
 /* This class demonstrates a simple MR (MapReduce) to count the number of words
 	in a directory of files on a distributed file system (HDFS).
 	The basic sequence of inputs and outputs for an MR job
-	(input) <k1, v1> -> MAP -> <k2, v2> -> COMBINE -> <k2,v2> -> 
-	REDUCE -> <k3, v3> (ouput)
+	(input) <k1, v1> -> MAP -> <k2, v2> -> COMBINE -> <k3,v3> -> 
+	REDUCE -> <k4, v4> (ouput)
 */ 
 public class WordCount {
 	public static class Map extends MapReduceBase
@@ -32,25 +32,30 @@ public class WordCount {
 				//set the writable Text word as the key k2
 				word.set(tokenizer.nextToken());
 				//add "one" to the value v2
+				//this may happend several times
 				output.collect(word, one);
 			}
 		}
 	}
 
+	//the COMBINE step is taken care of by the framework, basically it 
+	//collects the sorted output <k2,v2> from each mapper and combines 
+	//them into a unified <k3,v3> pairs where v3 is a collection of v2 values.
 	public static class Reduce extends MapReduceBase implements 
 	Reducer<Text, IntWritable, Text, IntWritable> {
-		// <k2, v2> -> REDUCE -> <k3, v3> (ouput)
+		// <k3, v3> -> REDUCE -> <k4, v4> (ouput)
 		public void reduce(Text key, Iterator<IntWritable> values, 
 		OutputCollector<Text, IntWritable> output, Reporter reporter)
 		throws IOException {
 			int sum = 0;
-		//each key can have multiple values to combine into the result
+		//each v3 can have multiple values
+		//we will loop through these values and combine them
 			while (values.hasNext()) {
 				//in our case we are adding the values, which are word counts for
 				//a particular Text key
 				sum += values.next().get();
 			}
-		//once this has been calculated, write it out to <k3, v3>
+		//once this has been calculated, write it out to <k4, v4>
 		output.collect(key, new IntWritable(sum));
 		}
 	}
